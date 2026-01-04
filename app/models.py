205ecb6a2f774b2aa4c2cc9d1e23e6696ed7c9f8
Muhammadali <baxtiyorovich779@gmail.com>
2026-01-04 22:07:58 +0500

    models update

--- app/models.py
+++ app/models.py
@@ -7,8 +7,8 @@ class Book(Base):
     __tablename__ = "books"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, nullable=False)
-    author = Column(String, nullable=False)
-    genre = Column(String, nullable=False)
+    title = Column(String(length=256), nullable=False)
+    author = Column(String(length=256), nullable=False)
+    genre = Column(String(length=128), nullable=False)
     year = Column(Integer, nullable=False)
     rating = Column(Float, nullable=False)
